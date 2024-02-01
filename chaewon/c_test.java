c98c22a28c71b0b4c84a13d0aa2e89c91447dd60
3uomlkh <cw1205@duksung.ac.kr>
2024-02-01 20:39:16 +0900

    Initial commit

---
+++ chaewon/c_test.java
@@ -0,0 +1,7 @@
+package chaewon;
+
+public class c_test {
+    public static void main(String[] args) {
+        System.out.println("test chae");
+    }
+}
---
+++ hyunjin/h_test.java
@@ -0,0 +1,7 @@
+package hyunjin;
+
+public class h_test {
+    public static void main(String[] args) {
+        System.out.println("test hyun");
+    }
+}
