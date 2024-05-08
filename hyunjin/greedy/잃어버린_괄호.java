518148c10076e271875d6379424d9400ca60f2ec
pearhyunjin <pearhyunjin@gmail.com>
2024-05-08 22:48:41 +0900

    fix: 잃어버린괄호 수정

--- hyunjin/greedy/잃어버린_괄호.java
+++ hyunjin/greedy/잃어버린_괄호.java
@@ -1,4 +1,33 @@
 package hyunjin.greedy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class 잃어버린_괄호 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> list = new ArrayList<>();
+        String str = br.readLine();
+        String[] strArr = str.split("-");
+
+        for (int i = 0; i < strArr.length; i++) {
+            int sum = 0;
+            String[] strArr2 = strArr[i].split("\\+");
+            for (int j = 0; j < strArr2.length; j++) {
+                sum += Integer.parseInt(strArr2[j]);
+            }
+            list.add(sum);
+        }
+
+        long answer = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            answer -= list.get(i);
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
 }
