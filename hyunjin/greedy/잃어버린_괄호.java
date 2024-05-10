00e259d1db4472e82d9870b2b37b6b9e25acd2bf
pearhyunjin <pearhyunjin@gmail.com>
2024-05-10 21:17:32 +0900

    fix: 잃어버린 괄호 수정 add: 사과담기 게임

--- hyunjin/greedy/잃어버린_괄호.java
+++ hyunjin/greedy/잃어버린_괄호.java
@@ -13,21 +13,32 @@ public class 잃어버린_괄호 {
         String str = br.readLine();
         String[] strArr = str.split("-");
 
+        // 30 - 15 + 40 = 30 - (15 + 40) = 30 - 55
+        // [30, 15 + 40]
         for (int i = 0; i < strArr.length; i++) {
             int sum = 0;
             String[] strArr2 = strArr[i].split("\\+");
+            // [30]
+            // [15, 40]
             for (int j = 0; j < strArr2.length; j++) {
                 sum += Integer.parseInt(strArr2[j]);
+                // 30
+                // 55
             }
             list.add(sum);
+            // {30}
+            // {30, 55}
         }
 
         long answer = list.get(0);
+        // 30
         for (int i = 1; i < list.size(); i++) {
             answer -= list.get(i);
+            // 30 - 55
         }
 
         System.out.println(answer);
+        // - 25
         br.close();
     }
 }
