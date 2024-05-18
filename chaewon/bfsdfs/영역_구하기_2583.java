eaee3a1175ba33d677ee66038539534e236094de
3uomlkh <cw1205@duksung.ac.kr>
2024-05-18 12:25:04 +0900

    add: 백준 BFS/DFS - 2583번 영역 구하기(주석 추가)

--- chaewon/bfsdfs/영역_구하기_2583.java
+++ chaewon/bfsdfs/영역_구하기_2583.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class 영역_구하기_2583 {
-    static int h, w, k, cnt, size;
+    static int h, w, k, size;
     static int[][] arr;
     static ArrayList<Integer> sizeResult;
     static int[] dx = {-1, 0, 0, 1};
@@ -26,6 +26,7 @@ public class 영역_구하기_2583 {
             int rx = Integer.parseInt(st.nextToken());
             int ry = Integer.parseInt(st.nextToken());
 
+            // 그림이 그려진 공간을 1로 채우기
             for (int y = ly; y < ry; y++) {
                 for (int x = lx; x < rx; x++) {
                     arr[y][x] = 1;
@@ -35,7 +36,7 @@ public class 영역_구하기_2583 {
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                if (arr[i][j] == 0) {
+                if (arr[i][j] == 0) { // 그림이 그려지지 않았다면 dfs
                     size = 1;
                     dfs(i, j);
                     sizeResult.add(size);
@@ -43,9 +44,11 @@ public class 영역_구하기_2583 {
             }
         }
 
+        // 오름차순 정렬
         Collections.sort(sizeResult);
         StringBuilder sb = new StringBuilder();
 
+        // 영역의 개수 == 사이즈를 담은 ArrayList의 크기
         sb.append(sizeResult.size()).append("\n");
         for (Integer size : sizeResult) {
             sb.append(size).append(" ");
@@ -55,15 +58,15 @@ public class 영역_구하기_2583 {
     }
 
     public static void dfs(int x, int y) {
-        arr[x][y] = 1;
+        arr[x][y] = 1; // 영역에 들어온 것을 표시하기 위해 1로 바꿈
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { // 상, 하, 좌, 우를 돌며 영역 체크
             int nowX = x + dx[i];
             int nowY = y + dy[i];
 
             if (nowX < 0 || nowX >= h || nowY < 0 || nowY >= w) continue;
-            if (arr[nowX][nowY] == 0) {
-                size++;
+            if (arr[nowX][nowY] == 0) { // 영역이 비어있다면
+                size++; // 사이즈를 하나 증가시키고 다시 dfs
                 dfs(nowX, nowY);
             }
         }
