620567da9cdc760350204748cffb64c1652115da
3uomlkh <cw1205@duksung.ac.kr>
2024-06-21 17:12:50 +0900

    add: 백준 최단경로 - 1058번 친구

---
+++ chaewon/shortest_path/친구_1058.java
@@ -0,0 +1,42 @@
+package chaewon.shortest_path;
+
+import java.io.*;
+
+public class 친구_1058 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[][] friend = new char[N][N];
+        boolean[][] isFriend = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < N; j++) {
+                friend[i][j] = input.charAt(j);
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            int twoFriend = 0;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (friend[i][j] == 'Y') {
+                    isFriend[i][j] = true;
+                    for (int k = 0; k < N; k++) {
+                        if (friend[j][k] == 'Y') isFriend[i][k] = true;
+                    }
+                }
+            }
+
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (isFriend[i][j]) twoFriend++;
+            }
+
+            max = Math.max(max, twoFriend);
+        }
+
+        System.out.println(max);
+    }
+}
